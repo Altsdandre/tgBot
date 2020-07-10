e0f93cfce08ae6129f66679d64d1e43385c64de0
altsdandre <altsdandre@gmail.com>
2020-07-11 00:05:39 +0300

    add input checks

--- bot.py
+++ bot.py
@@ -18,21 +18,49 @@ def start_handler(message):
 def askAge(message):
 	chat_id = message.chat.id
 	text = message.text
-	# if not text.isdigit():
-	#     msg = bot.send_message(chat_id, 'Возраст должен быть числом, введите ещё раз.')
-	#     bot.register_next_step_handler(msg, askAge) #askSource
-	#     return
-	ending = text.lower()[-3:]
+
+	text = text.strip()
+	if not text.isalpha():
+		msg = bot.send_message(chat_id, 'Ху*вое имечко...')
+		return
+
+	text = text.lower()
+	ending = text[-3:]
 	sign = ending[0]
 	
-	if sign == 'а':
+	if len(text) < 2:
+		msg = bot.send_message(chat_id, 'Ху*вое имечко...')
+		return
+	elif len(text) == 2:
+		if text[0] == 'у':
+			ending = 'ю' + text[1]
+		else:
+			ending = text
+	elif sign == 'а':
 		ending = 'я' + ending[-2:]
-	if sign in consonant:
-		ending = 'я' + ending
+	elif sign == 'о':
+		ending = 'ё' + ending[-2:]
+	elif sign in consonant:
+	precons = text[-4:-3]
+	signcons = text[-5:-4]
+	if precons in consonant:
+		ending = precons + ending
+		if signcons == 'а':
+			ending = 'я' + ending
+		elif signcons == 'у':
+			ending = 'ю' + ending
+		else:
+			ending = signcons + ending
+	else:
+		if precons == 'а':
+			ending = 'я' + ending
+		elif precons == 'у':
+			ending = 'ю' + ending
+		else:
+			ending =  precons + ending
 	
 	huname = 'Ху' + ending
 	msg = bot.send_message(chat_id, huname)
-
 
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
